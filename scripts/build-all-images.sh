e0bffe682596830011beb512f954c7a9e8267d38
Austin Clark <asclark109@gmail.com>
2022-11-29 22:38:41 -0600

    add no-caching; add auction-start shell script

--- scripts/build-all-images.sh
+++ scripts/build-all-images.sh
@@ -42,76 +42,51 @@ echo
 # 'docker build -t IMAGETAGNAME:VERSION PATHTODOCKERFILE'
 
 # auctions-service
-# export POSTGRES_IMG_DOCKERFILE="$PROJECT_DIR_PATH/$AUCTIONS_SERVICE_DIR_NAME/postgres/" # arbitrarily use dockerfile for postgres located in auctions repo
-# export AUCTIONS_POSTGRES_IMG_NAME="postgres-server"
-# echo POSTGRES_IMG_DOCKERFILE=$POSTGRES_IMG_DOCKERFILE
-# echo AUCTIONS_POSTGRES_IMG_NAME=$AUCTIONS_POSTGRES_IMG_NAME
 echo "building Postgres image ($AUCTIONS_POSTGRES_IMG_NAME) from dockerfile..."
-docker build -t "$AUCTIONS_POSTGRES_IMG_NAME:latest" $AUCTIONS_POSTGRES_IMG_DOCKERFILE
-echo
+docker build -t "$AUCTIONS_POSTGRES_IMG_NAME:latest" $AUCTIONS_POSTGRES_IMG_DOCKERFILE --no-cache
 
-# export RABBITMQ_IMG_DOCKERFILE="$PROJECT_DIR_PATH/$AUCTIONS_SERVICE_DIR_NAME/rabbitmq/"
-# export RABBITMQ_IMG_NAME="rabbitmq-server"
-# echo RABBITMQ_IMG_DOCKERFILE=$RABBITMQ_IMG_DOCKERFILE
-# echo RABBITMQ_IMG_NAME=$RABBITMQ_IMG_NAME
+# auctions-service
 echo "building RabbitMQ image ($RABBITMQ_IMG_NAME) from dockerfile..."
-docker build -t "$RABBITMQ_IMG_NAME:latest" $RABBITMQ_IMG_DOCKERFILE
-echo
+docker build -t "$RABBITMQ_IMG_NAME:latest" $RABBITMQ_IMG_DOCKERFILE --no-cache
 
-# export AUCTIONS_IMG_DOCKERFILE="$PROJECT_DIR_PATH/$AUCTIONS_SERVICE_DIR_NAME/auctions-service/"
-# export AUCTIONS_IMG_NAME="auctions-service"
-# echo AUCTIONS_IMG_DOCKERFILE=$AUCTIONS_IMG_DOCKERFILE
-# echo AUCTIONS_IMG_NAME=$AUCTIONS_IMG_NAME
+# auctions-service
 echo "building AuctionsService ($AUCTIONS_IMG_NAME) image from dockerfile..."
-docker build -t "$AUCTIONS_IMG_NAME:latest" $AUCTIONS_IMG_DOCKERFILE
-echo
+docker build -t "$AUCTIONS_IMG_NAME:latest" $AUCTIONS_IMG_DOCKERFILE  --no-cache
 
 # closed-auction-metrics
-
-# export CAM_MONGO_IMG_DOCKERFILE="$PROJECT_DIR_PATH/$CLOSED_AUCTION_METRICS_SERVICE_DIR_NAME/CAM_MONGOdb/"
-# export CAM_MONGO_IMG_NAME="cam-mongo-server"
-# echo CAM_MONGO_IMG_DOCKERFILE=$CAM_MONGO_IMG_DOCKERFILE
-# echo CAM_MONGO_IMG_NAME=$CAM_MONGO_IMG_NAME
 echo "building CAM_MONGO image ($CAM_MONGO_IMG_NAME) from dockerfile..."
-docker build -t "$CAM_MONGO_IMG_NAME:latest" $CAM_MONGO_IMG_DOCKERFILE
-echo
+docker build -t "$CAM_MONGO_IMG_NAME:latest" $CAM_MONGO_IMG_DOCKERFILE --no-cache
 
-# export CAM_IMG_DOCKERFILE="$PROJECT_DIR_PATH/$CAM_SERVICE_DIR_NAME/closed-auction-metrics/"
-# export CAM_IMG_NAME="cam-service"
-# echo CAM_IMG_DOCKERFILE=$CAM_IMG_DOCKERFILE
-# echo CAM_IMG_NAME=$CAM_IMG_NAME
+# closed-auction-metrics
 echo "building ClosedAuctionMetricsService ($CAM_IMG_NAME) image from dockerfile..."
-docker build -t "$CAM_IMG_NAME:latest" $CAM_IMG_DOCKERFILE
-echo
-
+docker build -t "$CAM_IMG_NAME:latest" $CAM_IMG_DOCKERFILE --no-cache
 
 # user-service
 echo "building user-service image ($USER_SERVICE_IMG_NAME) from dockerfile..."
-docker build -t $USER_SERVICE_IMG_NAME $USER_SERVICE_IMG_DOCKERFILE
-# docker build -t maven/builder:user-service $USER_SERVICE_IMG_DOCKERFILE
+docker build -t $USER_SERVICE_IMG_NAME $USER_SERVICE_IMG_DOCKERFILE --no-cache
 
 # watchlist-service
 echo "building watchlist-service image ($WATCHLIST_IMG_NAME) from dockerfile..."
-docker build -t $WATCHLIST_IMG_NAME $WATCHLIST_IMG_DOCKERFILE
+docker build -t $WATCHLIST_IMG_NAME $WATCHLIST_IMG_DOCKERFILE --no-cache
 
 # user-service
 echo "building user-service image ($ITEM_IMG_NAME) from dockerfile..."
-docker build -t $ITEM_IMG_NAME $ITEM_IMG_DOCKERFILE
+docker build -t $ITEM_IMG_NAME $ITEM_IMG_DOCKERFILE --no-cache
 
 # notification-service
 echo "building notification-service image ($NOTIFICATION_IMG_NAME) from dockerfile..."
-docker build -t $NOTIFICATION_IMG_NAME $NOTIFICATION_IMG_DOCKERFILE
+docker build -t $NOTIFICATION_IMG_NAME $NOTIFICATION_IMG_DOCKERFILE --no-cache
 
 # shopping-cart-service
 echo "building shopping-cart-service image ($SHOPPING_CART_IMG_NAME) from dockerfile..."
-docker build -t $SHOPPING_CART_IMG_NAME $SHOPPING_CART_IMG_DOCKERFILE
+docker build -t $SHOPPING_CART_IMG_NAME $SHOPPING_CART_IMG_DOCKERFILE --no-cache
 
 # gateway
 echo "building gateway image ($GATEWAY_IMG_NAME) from dockerfile..."
-docker build -t $GATEWAY_IMG_NAME $GATEWAY_IMG_DOCKERFILE
+docker build -t $GATEWAY_IMG_NAME $GATEWAY_IMG_DOCKERFILE --no-cache
 
 # # gateway-python
 # echo "building gateway-python image ($GATEWAY_PY_IMG_NAME) from dockerfile..."
-# docker build -t $GATEWAY_PY_IMG_NAME $GATEWAY_PY_IMG_DOCKERFILE
+# docker build -t $GATEWAY_PY_IMG_NAME $GATEWAY_PY_IMG_DOCKERFILE --no-cache
 
 echo "done"
